6cb2e40ce94441c97ee808a10f86fd259a4116eb
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-09-27 07:51:40 +0530

    added:E-mail id

--- src/RegexProblem.java
+++ src/RegexProblem.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class RegexProblem {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -14,6 +15,13 @@ public class RegexProblem {
         System.out.print("Enter last name: ");
         boolean lastName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
         System.out.println("Result : " + lastName);
-    }
 
+        System.out.println(" ");
+
+        System.out.print("Enter your e-mail id: ");
+        boolean email = Pattern.matches("[a-zA-Z0-9]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", sc.next());
+        System.out.println("Result : " + email);
+
+        System.out.println(" ");
+    }
 }
