04f1d1cf9e24ff28bb833649c7ddeaaafa36af43
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-09-27 08:02:01 +0530

    added:Upper Case

--- src/RegexProblem.java
+++ src/RegexProblem.java
@@ -4,45 +4,49 @@ import java.util.regex.Pattern;
 public class RegexProblem {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Scanner scanner =new Scanner(System.in);
+
+            Scanner sc = new Scanner(System.in);
+            Scanner scanner =new Scanner(System.in);
         /*
         First name starts with Cap and has minimum 3 characters
          */
-        System.out.print("Enter First name: ");
-        boolean firstName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
-        System.out.println("Result : " + firstName);
+            System.out.print("Enter First name: ");
+            boolean firstName = Pattern.matches("[A-Z][a-z]{2,}", sc.next());
+            System.out.println("Result : " + firstName);
 
-        System.out.println(" ");
+            System.out.println(" ");
         /*
         Last name starts with Cap and has minimum 3 characters
          */
-        System.out.print("Enter last name: ");
-        boolean lastName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
-        System.out.println("Result : " + lastName);
+            System.out.print("Enter last name: ");
+            boolean lastName = Pattern.matches("[A-Z][a-z]{2,}", sc.next());
+            System.out.println("Result : " + lastName);
 
-        System.out.println(" ");
+            System.out.println(" ");
         /*
          Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
          */
-        System.out.print("Enter your e-mail id: ");
-        boolean email = Pattern.matches("[a-zA-Z0-9.]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", sc.next());
-        System.out.println("Result : " + email);
+            System.out.print("Enter your e-mail id: ");
+            boolean email = Pattern.matches("[a-zA-Z0-9.]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", sc.next());
+            System.out.println("Result : " + email);
 
-        System.out.println(" ");
+            System.out.println(" ");
         /*
          Country code follow by space and 10 digit number
          */
 
-      /*  System.out.print("Enter your Mobile number: ");
-        boolean mobileNo = Pattern.matches("[0-9]{1,15}\s[9876]{1}[0-9]{9}",scanner.nextLine());
-        System.out.println("Result : " + mobileNo);
 
-        System.out.println(" ");
+            System.out.print("Enter your Mobile number: ");
+            */
+            boolean mobileNo = Pattern.matches("[0-9]{1,3}\s[9876]{1}[0-9]{9}",scanner.nextLine());
+            System.out.println("Result : " + mobileNo);
+
+            System.out.println(" ");
         /*
-        Password must be minimum of 8 character
+        Password must be minimum of 8 character.
+        at least one upper-case.
          */
-        System.out.print("Enter your Password: ");
-        boolean password = Pattern.matches("[a-zA-Z0-9]{8,}", sc.next());
-        System.out.println("Result : " + password);
-    }}
+            System.out.print("Enter your Password: ");
+            boolean password = Pattern.matches("^(?=.*[a-z])(?=.*[A-Z]).{8,20}$", sc.next());
+            System.out.println("Result : " + password);
+        }}
