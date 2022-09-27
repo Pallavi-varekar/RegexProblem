78caaf0b64b0ec5f2547af1d105629eaf279f8e0
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-09-27 07:49:21 +0530

    added:Last  name

--- src/RegexProblem.java
+++ src/RegexProblem.java
@@ -5,8 +5,15 @@ public class RegexProblem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter First name: ");
+        System.out.print("Enter First name: ");
         boolean firstName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
         System.out.println("Result : " + firstName);
+
+        System.out.println(" ");
+
+        System.out.print("Enter last name: ");
+        boolean lastName = Pattern.matches("[A-Z][a-z]{3,}", sc.next());
+        System.out.println("Result : " + lastName);
     }
+
 }
